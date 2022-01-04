735253f51bf634f2b73893791562105d1730e8bb
Alex <shoppingplaneta@gmail.com>
2022-01-04 16:45:12 +0200

    feat: done task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+  const str = String(n);
+
+  if (n > 0) {
+      return str.split('').reverse().join('')
+  } else {
+      return str.split('').slice(1).reverse().join('')
+  }
+
 }
